e0576a40fbb84a28c06a5758da30b71b889aed1f
antik1801 <antik.edu@gmail.com>
2023-06-13 08:34:27 +0600

    main

--- src/Pages/Dashboard/User/Payment/Payment.jsx
+++ src/Pages/Dashboard/User/Payment/Payment.jsx
@@ -1,8 +1,14 @@
+import Perallax from "../../../../components/Shared/Perallax";
 
 const Payment = () => {
     return (
+        <div className="w-full">
+            <div className="object-cover xl:-mt-40">
+            <Perallax heading={"Payment"} img="ashneer.webp"></Perallax>
+            </div>
         <div className="flex flex-col justify-center items-center text-center">
             <p className="text-3xl font-extrabold">Payment</p>
+        </div>
         </div>
     );
 };
