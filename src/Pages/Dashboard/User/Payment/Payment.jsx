394e39bf26786d46f9775e1721158bc2d2b66c69
antik1801 <antik.edu@gmail.com>
2023-06-13 09:19:19 +0600

    main

--- src/Pages/Dashboard/User/Payment/Payment.jsx
+++ src/Pages/Dashboard/User/Payment/Payment.jsx
@@ -1,13 +1,21 @@
+import { loadStripe } from "@stripe/stripe-js";
 import Perallax from "../../../../components/Shared/Perallax";
+import { Elements } from "@stripe/react-stripe-js";
+import Checkoutform from "../../../../components/Shared/Checkoutform";
 
+// TODO: provide publishable key
+const stripePromise=loadStripe(import.meta.env.VITE_PUBLISHABLE_KEY)
 const Payment = () => {
     return (
         <div className="w-full">
-            <div className="object-cover xl:-mt-40">
+            <div className="object-cover">
             <Perallax heading={"Payment"} img="ashneer.webp"></Perallax>
             </div>
-        <div className="flex flex-col justify-center items-center text-center">
+        <div className=" justify-center items-center text-center">
             <p className="text-3xl font-extrabold">Payment</p>
+            <Elements stripe={stripePromise}>
+                <Checkoutform></Checkoutform>
+            </Elements>
         </div>
         </div>
     );
--- src/components/Shared/Checkoutform.jsx
+++ src/components/Shared/Checkoutform.jsx
@@ -1,1 +1,46 @@
-rsc
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+
+const Checkoutform = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async event =>{
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return 
+        }
+        const card = elements.getElement(CardElement);
+        if (card === null) {
+            return
+        }
+
+
+    }
+    return (
+        <form onSubmit={handleSubmit} className="w-2/3 mx-auto my-10 border-2 p-5 border-blue-400 shadow-xl rounded-xl">
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button type="submit" disabled={!stripe} className="btn btn-primary mt-5">
+        Pay
+      </button>
+    </form>
+    );
+};
+
+export default Checkoutform;
